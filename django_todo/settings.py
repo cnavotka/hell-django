55b3b8e5c8a46c03367d573e5142eb44fd6bb49d
Carlos Navotka <knavotka@hotmail.com>
2021-09-25 18:28:25 +0000

    Set up enviroment variables and automatic deployment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@p(r^%fkb6&f=pu-ga%ez&otfu(#y8+11b-en&p)!)87$hurrt'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-@p(r^%fkb6&f=pu-ga%ez&otfu(#y8+11b-en&p)!)87$hurrt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['cn-django-todo-app.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
@@ -84,7 +85,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 
 DATABASES = {
-    'default': dj_database_url.parse('postgres://fzproeesihkxtf:85c334ab7a8e0b060c4f83bdffaacfbc64e35151c3873ce422d933930fbf5172@ec2-54-74-77-126.eu-west-1.compute.amazonaws.com:5432/de0mr2f1pdhrdq')
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
